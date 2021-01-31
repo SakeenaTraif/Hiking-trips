3b9d3d756f14b52c8c7f1bf70dc637aa40854a20
SakeenaTraif <s.traif30@gmail.com>
2021-01-31 09:59:54 +0300

    Hiking-trips

---
+++ src/hikingList.js
@@ -0,0 +1,25 @@
+const HikingList =[
+    {name:"Wonderland Trail",
+    image:"https://cdn.discordapp.com/attachments/801079262366400522/805328204172492821/image0.jpg",
+    city:"Mount Rainier National Park, Washington",
+    difficulty:"difficult",
+    length:"93-mile loop",
+    rating: "5",
+},
+{name:"Kalalau Trail",
+    image:"https://www.outsideonline.com/sites/default/files/styles/img_600x339/public/2019/03/15/kalalau-trail-kauai_h.jpg?itok=VvmwXsI5",
+    city:"Kauai, Hawaii",
+    difficulty:"medium",
+    length:"22 miles round-trip ",
+    rating: "3",
+},
+{name:"South Maroon Peak",
+    image:"https://www.outsideonline.com/sites/default/files/styles/img_600x339/public/2019/03/15/maroon-trail-colorado1_h.jpg?itok=JZxKDb90",
+    city:"Maroon Bells, Colorado",
+    difficulty:"Easy",
+    length:"12 miles round-trip",
+    rating: "1",
+}
+]
+
+export default HikingList;
